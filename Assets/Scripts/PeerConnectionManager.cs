8f8090b6f3bafa05480999345509886193f47ccd
Daekun Kim <dkkim6200@gmail.com>
2020-08-23 20:01:14 -0700

    Initial commit

---
+++ Assets/Scripts/PeerConnectionManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.WebRTC.Unity;
+
+public class PeerConnectionManager : MonoBehaviour
+{
+    public void StartConnection()
+    {
+        GetComponent<PeerConnection>().StartConnection();
+    }
+}
